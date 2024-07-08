8aa8ab5ea4fdb9fd5f6e1d22b3c8a8c8d55ddd67
yauz3 <s.yavuz.ugurlu@gmail.com>
2024-07-09 00:14:09 +0300

    mega_protac

--- bin/voronota/bin/src/scripting/operators_common.h
+++
@@ -1,7 +0,0 @@
-#ifndef SCRIPTING_OPERATORS_COMMON_H_
-#define SCRIPTING_OPERATORS_COMMON_H_
-
-#include "operators_base.h"
-#include "operators_utilities.h"
-
-#endif /* SCRIPTING_OPERATORS_COMMON_H_ */
